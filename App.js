5960bfa5eb3ee7ac9271fb3426eac5ccb212f005
wind_create <nardowilli@gmail.com>
2022-01-19 04:57:32 +0700

    make homescreen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/Home'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar hidden />
+      <HomeScreen />
     </View>
   );
 }
---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import styles from './styles'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>ini homescreen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
