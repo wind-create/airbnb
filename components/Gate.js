d79b1da32c231233262cec098d868827b144253d
wind_create <nardowilli@gmail.com>
2022-01-21 07:10:36 +0700

    fix store middleware

--- components/Gate.js
+++ components/Gate.js
@@ -11,7 +11,7 @@ export default () => {
   const dispatch = useDispatch();
   return (
     <NavigationContainer>
-      {isLoggedIn ? <Main /> : <Main />}
+      {isLoggedIn ? <Main /> : <Auth />}
     </NavigationContainer>
   );
 };
