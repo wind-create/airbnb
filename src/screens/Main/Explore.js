f9792d516b49a0ba4793a529e2646cc7fa86974b
wind_create <nardowilli@gmail.com>
2022-01-22 16:41:12 +0700

    explore search tab

--- src/screens/Main/Explore.js
+++ src/screens/Main/Explore.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  StatusBar
+} from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const Explore = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <View style={{ backgroundColor: 'white', borderBottomWidth: 1, borderBottomColor: '#dddddd' }}>
+          <View style={{
+            flexDirection: 'row', padding: 10,
+            backgroundColor: 'white', marginHorizontal: 20,
+            shadowOffset: { width: 0, height: 0 },
+            shadowColor: 'black',
+            shadowOpacity: 0.2,
+            elevation: 1
+          }}>
+            <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
+            <TextInput
+              underlineColorAndroid="transparent"
+              placeholder="Try Indonesia"
+              placeholderTextColor="grey"
+              style={{ flex: 1, fontWeight: '700', backgroundColor: 'white' }}
+            />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 };
 
