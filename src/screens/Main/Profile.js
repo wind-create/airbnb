a1f2eaf39ea5d178a1c4d9eeb0a25de1885e5029
wind_create <nardowilli@gmail.com>
2022-01-23 06:38:15 +0700

    make Login

--- src/screens/Main/Profile.js
+++ src/screens/Main/Profile.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Login from '../../components/organisms/login';
 
 const Profile = () => {
   return (
     <View>
-      <Text>Profile</Text>
+      <Login />
     </View>
   )
 };
