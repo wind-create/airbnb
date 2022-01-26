8cfd9a915f0fa23e0ec0ad91482f4ce9f359c935
wind_create <nardowilli@gmail.com>
2022-01-26 13:49:05 +0700

    setting

--- src/screens/Main/Setting.js
+++ src/screens/Main/Setting.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Setting({ setToken }) {
   return (
     <View>
       <Text>Hello Settings</Text>
 
-      <Button
-        title="Log Out"
-        onPress={() => {
-          setToken(null);
-        }}
-      />
     </View>
   );
 }
