624860bcad676db3990cd0c21b8917585b5e649d
wind_create <nardowilli@gmail.com>
2022-01-30 05:33:33 +0700

    whishlist feature

--- src/screens/Main/Saved.js
+++ src/screens/Main/Saved.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Fragment } from 'react-native';
+import { useSelector } from "react-redux";
+import CardLarge from '../../components/atom/cardLarge';
 
 const Saved = () => {
+  const user = useSelector(
+    (state) => state.user.user
+  );
+  const savedHotel = useSelector(
+    (state) => state.user.user.whishlist
+  );
   return (
-    <View>
-      <Text>Saved</Text>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+
+      {user.loggedIn ? (
+        savedHotel.map((hotel, index) => (
+          <CardLarge key={index} hotel={hotel.hotel} />
+        ))
+      ) : (
+        <Text>Please login</Text>
+      )}
+
+    </ScrollView>
   )
 };
 
