47e80b37d917322b7b56c6edc4d569a25645991f
wind_create <nardowilli@gmail.com>
2022-01-29 14:54:27 +0700

    edit profile

--- src/redux/User.js
+++ src/redux/User.js
@@ -7,18 +7,20 @@ export const user = createSlice({
         user: {
             name: "Willi Nardo",
             country: "Indonesia",
-            username: "user",
             email: "willi@gmail.com",
             password: "12345",
             loggedIn: true,
         },
     },
     reducers: {
-        setname: (state, action) => {
+        setName: (state, action) => {
             state.user.name = action.payload;
         },
         setEmail: (state, action) => {
             state.user.email = action.payload;
+        },
+        setCountry: (state, action) => {
+            state.user.country = action.payload;
         },
         LogIn: (state, action) => {
             state.user.loggedIn = action.payload;
--- src/screens/Main/EditProfile.js
+++ src/screens/Main/EditProfile.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { useDispatch, useSelector } from "react-redux";
+import { setName, setEmail, setCountry } from '../../redux/User';
 
 const EditPRofile = () => {
+    const dispatch = useDispatch();
+    const EditUser = useSelector((state) => state.user.user);
     return (
-        <View>
-            <Text>Edit Profile</Text>
+        <View style={styles.screen}>
+            <Text> Edit Profile </Text>
+            <TextInput
+                style={styles.textInput}
+                placeholder="Name"
+                placeholderTextColor="#666666"
+                onChangeText={(text) => dispatch(setName(text))}
+                value={EditUser.name}
+            />
+            <TextInput
+                style={styles.textInput}
+                placeholder="Email"
+                placeholderTextColor="#666666"
+                onChangeText={(text) => dispatch(setEmail(text))}
+                value={EditUser.email}
+            />
+            <TextInput
+                style={styles.textInput}
+                placeholder="Country"
+                placeholderTextColor="#666666"
+                onChangeText={(text) => dispatch(setCountry(text))}
+                value={EditUser.country}
+            />
+            <TextInput
+                style={styles.textInput}
+                placeholder="Country"
+                placeholderTextColor="#666666"
+                onChangeText={(text) => dispatch(setCountry(text))}
+                value={EditUser.country}
+            />
         </View>
     )
 };
-
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        alignItems: "center",
+        paddingTop: 50
+    },
+    textInput: {
+        paddingLeft: 10,
+        color: '#333333',
+    },
+})
 export default EditPRofile;
